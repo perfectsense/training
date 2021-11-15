60eb416f04c48f0801e8c86fd6a267ba062ce79a
Brennan Ebeling <brennan@brightspot.com>
2021-11-15 15:59:47 -0500

    Keep denormalized RecipeTag name up-to-date

--- core/src/main/java/brightspot/recipe/RecipeTag.java
+++ core/src/main/java/brightspot/recipe/RecipeTag.java
@@ -1,5 +1,8 @@
 package brightspot.recipe;
 
+import java.util.Objects;
+
+import brightspot.recalculate.MethodRecalculation;
 import brightspot.rte.TinyRichTextToolbar;
 import brightspot.util.MoreStringUtils;
 import brightspot.util.NoUrlsWidget;
@@ -7,9 +10,12 @@ import brightspot.util.RichTextUtils;
 import com.psddev.cms.db.Content;
 import com.psddev.cms.db.ToolUi;
 import com.psddev.cms.ui.form.DynamicPlaceholderMethod;
+import com.psddev.dari.db.Query;
 
 public class RecipeTag extends Content implements
     NoUrlsWidget {
+
+    private static final String NAME_MODIFIED_EXTRA = RecipeTag.class.getName() + ".nameModified";
 
     @Indexed
     @Required
@@ -41,6 +47,43 @@ public class RecipeTag extends Content implements
 
     private String getInternalNameFallback() {
         return RichTextUtils.richTextToPlainText(getName());
+    }
+
+    // --- Record support ---
+
+    @Override
+    protected void beforeCommit() {
+        Object previousVersion = Query.fromAll()
+            .where("_id = ?", this)
+            .noCache()
+            .first();
+        if (!(previousVersion instanceof RecipeTag)) {
+            return;
+        }
+
+        RecipeTag previousRecipeTag = (RecipeTag) previousVersion;
+
+        if (!Objects.equals(previousRecipeTag.getName(), this.getName())) {
+            getState().getExtras().put(NAME_MODIFIED_EXTRA, true);
+        }
+    }
+
+    @Override
+    protected void afterSave() {
+        if (Boolean.TRUE.equals(getState().getExtra(NAME_MODIFIED_EXTRA))) {
+            MethodRecalculation.recalculateMethodByQuery(
+                Query.fromAll()
+                    .where(Recipe.class.getName() + "/" + Recipe.RECIPE_TAGS_FIELD + " = ?", this),
+                Recipe.RECIPE_TAG_NAMES_FIELD);
+        }
+    }
+
+    @Override
+    protected void afterDelete() {
+        MethodRecalculation.recalculateMethodByQuery(
+            Query.fromAll()
+                .where(Recipe.class.getName() + "/" + Recipe.RECIPE_TAGS_FIELD + " = ?", this),
+            Recipe.RECIPE_TAG_NAMES_FIELD);
     }
 
     // --- Recordable support ---
