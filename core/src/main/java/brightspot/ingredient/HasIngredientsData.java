6b33ccb82fbeafb5cc04fc8ef3a8bcc17f50e627
Brennan Ebeling <brennan@brightspot.com>
2024-07-29 11:53:25 -0400

    5i. Denormalize ingredient names onto Recipe

--- core/src/main/java/brightspot/ingredient/HasIngredientsData.java
+++ core/src/main/java/brightspot/ingredient/HasIngredientsData.java
@@ -1,6 +1,8 @@
 package brightspot.ingredient;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.psddev.cms.db.ToolUi;
 import com.psddev.dari.db.Modification;
@@ -12,10 +14,20 @@ public class HasIngredientsData extends Modification<HasIngredients> {
 
     static final String FIELD_PREFIX = "hasIngredients.";
     public static final String INGREDIENTS_FIELD = FIELD_PREFIX + "getIngredients";
+    public static final String INGREDIENT_NAMES_FIELD = FIELD_PREFIX + "getIngredientNames";
 
     @Indexed
     @ToolUi.Hidden
     public List<Ingredient> getIngredients() {
         return UnresolvedState.resolveAndGet(getOriginalObject(), HasIngredients::getIngredients);
+    }
+
+    @Indexed
+    @ToolUi.Hidden
+    public Set<String> getIngredientNames() {
+        return getIngredients()
+            .stream()
+            .map(Ingredient::getNamePlainText)
+            .collect(Collectors.toSet());
     }
 }
--- core/src/main/java/brightspot/ingredient/Ingredient.java
+++ core/src/main/java/brightspot/ingredient/Ingredient.java
@@ -1,15 +1,20 @@
 package brightspot.ingredient;
 
+import java.util.Objects;
+
+import brightspot.recalculate.MethodRecalculation;
 import brightspot.rte.TinyRichTextToolbar;
 import brightspot.util.NoUrlsWidget;
 import brightspot.util.RichTextUtils;
 import com.psddev.cms.db.Content;
 import com.psddev.cms.db.ToolUi;
+import com.psddev.dari.db.Query;
 
 public class Ingredient extends Content implements
     NoUrlsWidget {
 
     public static final String NAME_PLAIN_TEXT_FIELD = "getNamePlainText";
+    private static final String NAME_MODIFIED_EXTRA = Ingredient.class.getName() + ".nameModified";
 
     @ToolUi.RichText(toolbar = TinyRichTextToolbar.class)
     private String name;
@@ -33,6 +38,45 @@ public class Ingredient extends Content implements
     @ToolUi.Hidden
     public String getNamePlainText() {
         return RichTextUtils.richTextToPlainText(getName());
+    }
+
+    // --- Record support ---
+
+    @Override
+    protected void beforeCommit() {
+        Object previousVersion = Query.fromAll()
+            .where("_id = ?", this)
+            .noCache()
+            .first();
+        if (!(previousVersion instanceof Ingredient)) {
+            return;
+        }
+
+        Ingredient previousIngredient = (Ingredient) previousVersion;
+
+        if (!Objects.equals(previousIngredient.getNamePlainText(), this.getNamePlainText())
+            || !Objects.equals(previousIngredient.getState().isVisible(), this.getState().isVisible())) {
+
+            getState().getExtras().put(NAME_MODIFIED_EXTRA, true);
+        }
+    }
+
+    @Override
+    protected void afterSave() {
+        if (Boolean.TRUE.equals(getState().getExtra(NAME_MODIFIED_EXTRA))) {
+            MethodRecalculation.recalculateMethodByQuery(
+                Query.fromAll()
+                    .where(HasIngredientsData.class.getName() + "/" + HasIngredientsData.INGREDIENTS_FIELD + " = ?", this),
+                HasIngredientsData.INGREDIENT_NAMES_FIELD);
+        }
+    }
+
+    @Override
+    protected void afterDelete() {
+        MethodRecalculation.recalculateMethodByQuery(
+            Query.fromAll()
+                .where(HasIngredientsData.class.getName() + "/" + HasIngredientsData.INGREDIENTS_FIELD + " = ?", this),
+            HasIngredientsData.INGREDIENT_NAMES_FIELD);
     }
 
     // --- Recordable support ---
