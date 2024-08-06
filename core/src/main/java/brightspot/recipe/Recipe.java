6500c78f668f5a2edc614c045a4b4387ab541430
Brennan Ebeling <brennan@brightspot.com>
2024-08-06 12:12:52 -0400

    1i. Add basic Recipe data model

---
+++ core/src/main/java/brightspot/recipe/Recipe.java
@@ -0,0 +1,124 @@
+package brightspot.recipe;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import brightspot.image.WebImage;
+import brightspot.rte.SmallRichTextToolbar;
+import brightspot.rte.TinyRichTextToolbar;
+import com.psddev.cms.db.Content;
+import com.psddev.cms.db.ToolUi;
+
+public class Recipe extends Content {
+
+    @Required
+    @ToolUi.RichText(toolbar = TinyRichTextToolbar.class)
+    private String title;
+
+    private String internalName;
+
+    @ToolUi.RichText(inline = false, toolbar = SmallRichTextToolbar.class)
+    private String description;
+
+    private WebImage image;
+
+    private Integer prepTime;
+
+    private Integer inactivePrepTime;
+
+    private Integer cookTime;
+
+    private Integer totalTimeOverride;
+
+    // --- Getters/setters ---
+
+    /**
+     * @return rich text
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInternalName() {
+        return internalName;
+    }
+
+    public void setInternalName(String internalName) {
+        this.internalName = internalName;
+    }
+
+    /**
+     * @return rich text
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public WebImage getImage() {
+        return image;
+    }
+
+    public void setImage(WebImage image) {
+        this.image = image;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getInactivePrepTime() {
+        return inactivePrepTime;
+    }
+
+    public void setInactivePrepTime(Integer inactivePrepTime) {
+        this.inactivePrepTime = inactivePrepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getTotalTimeOverride() {
+        return totalTimeOverride;
+    }
+
+    public void setTotalTimeOverride(Integer totalTimeOverride) {
+        this.totalTimeOverride = totalTimeOverride;
+    }
+
+    // --- API methods ---
+
+    public Integer getTotalTime() {
+        return Optional.ofNullable(getTotalTimeOverride())
+            .orElseGet(this::getTotalTimeFallback);
+    }
+
+    // --- Fallbacks ---
+
+    private Integer getTotalTimeFallback() {
+        return Stream.of(
+                getPrepTime(),
+                getInactivePrepTime(),
+                getCookTime())
+            .filter(Objects::nonNull)
+            .mapToInt(Integer::intValue)
+            .reduce(0, Integer::sum);
+    }
+}
